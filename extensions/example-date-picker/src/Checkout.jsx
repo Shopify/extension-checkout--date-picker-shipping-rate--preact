b3d8ea022234eb981aaf3a8e597b39efebca6a61
LucasLacerdaUX <lucas.lacerda@shopify.com>
2025-09-17 18:20:56 -0400

    Update to implement metafield logic + restrict to Express

--- extensions/example-date-picker/src/Checkout.jsx
+++ extensions/example-date-picker/src/Checkout.jsx
@@ -1,8 +1,12 @@
 import '@shopify/ui-extensions/preact';
 import {render} from "preact";
-import { useState } from "preact/hooks";
-import { useMemo } from "preact/hooks";
-import { useCallback } from "preact/hooks";
+import { useState, useCallback, useMemo } from "preact/hooks";
+ import {
+   useApplyMetafieldsChange,
+   useDeliveryGroupListTarget,
+   useApi,
+} from "@shopify/ui-extensions/checkout/preact";
+
 
 // 1. Export the extension
 export default function() {
@@ -12,6 +16,16 @@ export default function() {
 function Extension() {
   const [selectedDate, setSelectedDate] = useState("");
   const [yesterday, setYesterday] = useState("");
+
+  // Set a function to handle updating a metafield
+  const applyMetafieldsChange = useApplyMetafieldsChange();
+
+  // Get the delivery group list
+  const deliveryGroupList = useDeliveryGroupListTarget();
+
+  // Define the metafield namespace and key
+  const metafieldNamespace = "yourAppNamespace";
+  const metafieldKey = "deliverySchedule";
 
   // Sets the selected date to today, unless today is Sunday, then it sets it to tomorrow
   useMemo(() => {
@@ -31,10 +45,43 @@ function Extension() {
 
   // Set a function to handle the Date Picker component's onChange event
   const handleChangeDate = useCallback((event) => {
-    setSelectedDate(event.target.value);
+    const selectedDate = event.target.value;
+    setSelectedDate(selectedDate);
+
+    // Apply the change to the metafield
+    applyMetafieldsChange({
+      type: selectedDate ? "updateMetafield" : "removeMetafield",
+      namespace: metafieldNamespace,
+      key: metafieldKey,
+      valueType: "string",
+      value: selectedDate,
+    });
   }, []);
 
-  return (
+  // Guard against duplicate rendering of `shipping-option-list.render-after` target for one-time purchase and subscription sections. Calling `applyMetafieldsChange()` on the same namespace-key pair from duplicated extensions would otherwise cause an overwrite of the metafield value.
+  // Instead of guarding, another approach would be to prefix the metafield key when calling `applyMetafieldsChange()`. The `deliveryGroupList`'s `groupType` could be used to such effect.
+  if (!deliveryGroupList || deliveryGroupList.groupType !== 'oneTimePurchase') {
+    return null;
+  }
+
+  const { deliveryGroups } = deliveryGroupList;
+
+  // Function to compute if Express is selected in any of the delivery groups
+  let isExpressSelected = () => {
+    const expressHandles = new Set(
+      deliveryGroups
+        .map(
+          ({deliveryOptions}) =>
+            deliveryOptions.find(({ title }) => title === "Express")?.handle,
+        )
+        .filter(Boolean),
+    );
+    return deliveryGroups.some(({ selectedDeliveryOption }) =>
+      expressHandles.has(selectedDeliveryOption?.handle),
+    );
+  };
+
+  return (isExpressSelected() ? (
     <>
       <s-heading>Select a date for delivery</s-heading>
       <s-date-picker
@@ -44,7 +91,7 @@ function Extension() {
         disallowDays="sunday"
       />
     </>
-  )
+  ) : null);
 }
 
 const formatDate = (date) => {
